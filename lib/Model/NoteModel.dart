c50f54c40a8bed78eae0a6f03e61cb67510d953a
elsaid maher <elsiadmaher@students.du.edu.eg>
2025-03-13 12:45:55 +0200

    CreateModelTonoteApp

---
+++ lib/Model/NoteModel.dart
@@ -0,0 +1,10 @@
+class Notemodel {
+  Notemodel({
+    required this.Title,
+    required this.Subtitle,
+    required this.dateTime,
+  });
+  String Title;
+  String Subtitle;
+  DateTime dateTime;
+}
