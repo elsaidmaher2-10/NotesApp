e9b01a1bcb479d43c7ab1595fdd6bc00a0ec8608
elsaid maher <elsiadmaher@students.du.edu.eg>
2025-03-14 20:22:06 +0200

    Provide object from noteCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notesapp/Cubits/AddNoteCubit/AddNoteCubit.dart';
 import 'package:notesapp/Model/NoteModel.dart';
 import 'package:notesapp/Views/HomeScreen.dart';
 
@@ -19,17 +21,20 @@ class Notesapp extends StatelessWidget {
   Widget build(BuildContext context) {
     // ThemeData.dark() is used to make them dark
     //
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: homescreen(),
-      theme: ThemeData(
-        fontFamily: GoogleFonts.poppins().fontFamily, // ✅ الحل الصحيح
-        scaffoldBackgroundColor: Colors.grey[900],
+    return BlocProvider<AddNoteCubit>(
+      create: (context) => AddNoteCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: homescreen(),
+        theme: ThemeData(
+          fontFamily: GoogleFonts.poppins().fontFamily, // ✅ الحل الصحيح
+          scaffoldBackgroundColor: Colors.grey[900],
 
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(color: Colors.black),
-          bodyMedium: TextStyle(color: Colors.black),
-          bodySmall: TextStyle(color: Colors.black),
+          textTheme: TextTheme(
+            bodyLarge: TextStyle(color: Colors.black),
+            bodyMedium: TextStyle(color: Colors.black),
+            bodySmall: TextStyle(color: Colors.black),
+          ),
         ),
       ),
     );
