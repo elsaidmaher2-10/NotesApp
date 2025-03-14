847341fe83062a70b15e0bdbf08f9ea2df7dba07
elsaid maher <elsiadmaher@students.du.edu.eg>
2025-03-14 02:24:56 +0200

    createtypeadabtorfoenode

--- lib/Model/NoteModel.dart
+++ lib/Model/NoteModel.dart
@@ -1,12 +1,21 @@
-class Notemodel {
+import 'package:hive/hive.dart';
+part 'NoteModel.g.dart';
+
+// id for database
+@HiveType(typeId: 0)
+class Notemodel extends HiveObject {
   Notemodel({
     required this.Color,
     required this.Title,
     required this.Subtitle,
     required this.dateTime,
   });
+  @HiveField(0)
   String Title;
+  @HiveField(1)
   String Subtitle;
+  @HiveField(2)
   String dateTime;
+  @HiveField(3)
   int Color;
 }
